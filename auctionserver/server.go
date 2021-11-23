8d1e99431c9297c2e98ab278890e461cebe2b466
kaeppen <seho@itu.dk>
2021-11-23 10:54:05 +0100

    start implementing streaming between primary and backups

--- auctionserver/server.go
+++ auctionserver/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -15,10 +16,10 @@ import (
 type Server struct {
 	a.UnimplementedAuctionatorServer
 	Id         int32
-	isPrimary  bool                //am i primary
-	primary    a.AuctionatorServer //who is my primary
-	backups    map[int32]Server    //the backups
-	clients    map[int32]bool      //the registered clients - only known by id (bad?) -> can be something else than a map?
+	isPrimary  bool                                                    //am i primary
+	primary    a.AuctionatorServer                                     //who is my primary
+	backups    map[int32]a.Auctionator_EstablishBackupConnectionServer //the backups
+	clients    map[int32]bool                                          //the registered clients - only known by id (bad?) -> can be something else than a map?
 	highestBid Bid
 	isOver     bool
 	result     int32
@@ -49,18 +50,18 @@ func main() {
 
 }
 
-func (s *Server) notifyBackups() {
-	//loop over all my backups and update their maps to mine
-	for i := range s.backups {
-		//no need to clear out the map first, as it will only hold stuff that the primary also holds
-		for k, v := range s.responses {
-			s.backups[i].responses[k] = v
-		}
-	}
-}
+// func (s *Server) notifyBackups() {
+// 	//loop over all my backups and update their maps to mine
+// 	for i := range s.backups {
+// 		//no need to clear out the map first, as it will only hold stuff that the primary also holds
+// 		for k, v := range s.responses {
+// 			s.backups[i].responses[k] = v
+// 		}
+// 	}
+// }
 
 func (s *Server) setupServer() {
-	s.backups = make(map[int32]Server)
+	s.backups = make(map[int32]a.Auctionator_EstablishBackupConnectionServer)
 	//set the servers id from environment variable
 	id, _ := strconv.Atoi(os.Getenv("ID"))
 	s.Id = int32(id)
@@ -70,8 +71,41 @@ func (s *Server) setupServer() {
 		s.isPrimary = true
 	} else {
 		primary := os.Getenv("PRIMARY")
-		s.primary = nil //nil er pladsholder, vi skal sætte primary på ene eller anden måde :) måske kan den få en adresse/port ind denne vej
+		s.connectToPrimary(primary) //der skal måske laves lidt formattering her
+		//s.primary = nil //nil er pladsholder, vi skal sætte primary på ene eller anden måde :) måske kan den få en adresse/port ind denne vej
 	}
+}
+
+func (s *Server) connectToPrimary(port string) {
+	conn, err := grpc.Dial(port, grpc.WithBlock(), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect %s", err)
+	}
+	ctx := context.Background()
+	defer conn.Close()
+	primary := a.NewAuctionatorClient(conn)
+	setup := &a.ConnectionSetup{Port: "skal fjernes, da det jo ikke er nødvendigt", Id: s.Id}
+	stream, err := primary.EstablishBackupConnection(ctx, setup)
+	if err != nil {
+		log.Fatalf("Error while connecting to primray %v", err)
+	}
+	//listen on the stream, might have to be changed
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				log.Fatalf("Backup with id %v failed to recieve message", s.Id)
+			}
+			s.updateResponses(in)
+		}
+	}()
+}
+
+func (s *Server) updateResponses(response *a.UpdateResponse) {
+	s.responses[response.Key] = response.Value
 }
 
 func (s *Server) Bid(ctx context.Context, amount *a.Amount) (*a.Acknowledgement, error) {
