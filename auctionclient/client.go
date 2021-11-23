e1f45ad4ae2a284f1667a887b2825c87d19267c3
kaeppen <seho@itu.dk>
2021-11-23 09:58:14 +0100

    start implementing client

--- auctionclient/client.go
+++ auctionclient/client.go
@@ -1,22 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"os"
+	"strconv"
 
-	"github.com/kaeppen/disys-miniproject3/auctionator"
+	a "github.com/kaeppen/disys-miniproject3/auctionator"
 	"google.golang.org/grpc"
 )
 
+type Frontend struct {
+	//det er mig der er grpc klient
+	connection a.AuctionatorClient
+	ctx        context.Context
+}
+
+type Client struct {
+	Id    int32
+	front Frontend
+}
+
 func main() {
-	var port = "1337" //replace with somer docker stuff
+	c := Client{}
+	id, _ := strconv.Atoi(os.Getenv("ID"))
+	c.Id = int32(id)
+	c.setupFrontend()
+
+}
+
+//overvej om der skal returværdi på denne?
+func (c *Client) Bid(amount int32) {
+	input := &a.Amount{Amount: amount, ClientId: c.Id}
+	ack, err := c.front.connection.Bid(c.front.ctx, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Client %v got response %v", c.Id, ack.Ack)
+}
+
+func (c *Client) Result() {
+	outcome, err := c.front.connection.Result(c.front.ctx, &a.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Client %v: auction is over: %v and the highest bidder/result is: %v", c.Id, outcome.Over, outcome.Result)
+}
+
+func (c *Client) setupFrontend() {
 	var conn *grpc.ClientConn
+	log.Print("Trying to connect to server")
+	var port = os.Getenv("PORT")
 	conn, err := grpc.Dial(port, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("Client %v could not connect: %s", "klientnavn", err)
+		log.Fatalf("Could not connect: %s", err)
 	}
 	defer conn.Close()
-
-	client := auctionator.NewAuctionatorClient(conn)
-	fmt.Printf("client: %v\n", client) // placeholder line
+	ctx := context.Background()
+	client := a.NewAuctionatorClient(conn)
+	c.front.connection = client
+	c.front.ctx = ctx
 }
